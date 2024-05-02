134ddb0df3d8ccbad6bdac6e134b0ee31f6541de
The0ne0 <The0ne0>
2024-05-01 17:46:53 -0700

    onklic on menu finesedgit add *git add *git add *git add *git add *git add *git add *git add *git add *git add *git add *git add *git add *git add *!

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const menu = document.querySelector (".menu")
-const menu_link = document.querySelector (".menu-link")
+const menu_link = menu.querySelectorAll (".menu-link")
 
 
 function change_active (event){
-    console.log(event.target)
+    const target = event.target
+    if (target.classList.contains("menu-link")){
+        const prev_activ = menu.querySelector(".active");
+        prev_activ.classList.remove("active");
+        target.classList.add("active");
+    }
 }
 
 const swiper = new Swiper('.swiper', {
@@ -15,3 +20,26 @@ const swiper = new Swiper('.swiper', {
 });
 
 menu.onclick = change_active
+
+window.onscroll = () => {
+    const h =document.documentElement.clientHeight;
+    let new_active = null;
+    if(window.scrollY >= h * 4){
+        new_active =menu_link[4];
+    }
+    else if(window.scrollY >= h * 3){
+        new_active =menu_link[3];
+    }
+    else if(window.scrollY >= h * 2){
+        new_active =menu_link[2];
+    }
+    else if(window.scrollY >= h * 1){
+        new_active =menu_link[1];
+    }
+    else{
+        new_active =menu_link[0];
+    }
+    const prev_activ = menu.querySelector(".active");
+    prev_activ.classList.remove("active");
+    new_active.classList.add("active");
+};
