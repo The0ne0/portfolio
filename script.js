ceecf613d6aa712865e32ea99aad9a01435d6288
The0ne0 <The0ne0>
2024-05-08 17:40:48 -0700

    moregit add *git add *git add * mobile stuf and details

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const menu = document.querySelector (".menu")
 const menu_link = menu.querySelectorAll (".menu-link")
+const menu_btn = document.querySelector(".menu-button")
+const mobile_menu = document.querySelector(".mobile_menu")
+const mobile_link = mobile_menu.querySelectorAll (".menu-link")
 
 
 function change_active (event){
@@ -24,28 +27,36 @@ const swiper = new Swiper('.swiper', {
 window.onscroll = () => {
     const h =document.documentElement.clientHeight;
     let new_active = null;
+    let mobile_active = null;
     if(window.scrollY >= h * 4){
-        new_active =menu_link[4];
+        new_active = menu_link[4];
+        mobile_active = mobile_link[4];
     }
     else if(window.scrollY >= h * 3){
         new_active =menu_link[3];
+        mobile_active = mobile_link[3];
     }
     else if(window.scrollY >= h * 2){
         new_active =menu_link[2];
+        mobile_active = mobile_link[2];
     }
     else if(window.scrollY >= h * 1){
         new_active =menu_link[1];
+        mobile_active = mobile_link[1];
     }
     else{
         new_active =menu_link[0];
+        mobile_active = mobile_link[0];
     }
     const prev_activ = menu.querySelector(".active");
     prev_activ.classList.remove("active");
     new_active.classList.add("active");
+    const prev_mobile = mobile_menu.querySelector(".active");
+    prev_mobile.classList.remove("active");
+    mobile_active.classList.add("active");
 };
 
-const menu_btn = document.querySelector(".menu-button")
-const mobile_menu = document.querySelector(".mobile_menu")
+
 
 menu_btn.onclick = () => {
     mobile_menu.classList.toggle("hide");
